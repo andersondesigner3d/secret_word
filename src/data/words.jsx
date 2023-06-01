8c04aa8f748d20ec8b6f1d805235f5d0debe5ed6
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2023-06-01 15:19:03 -0300

    limpo app.jsx e todos os csss do projeto inicial

---
+++ src/data/words.jsx
@@ -0,0 +1,9 @@
+
+export const wordslist = {
+    carro : ["motor","porta","volante","marcha","freio"],
+    fruta : ["maçã","abacaxi","uva","manga","banana"],
+    corpo : ["braço","perna","cabeça","nariz","olho"],
+    computador : ["teclado","mouse","monitor","memória","headset"],
+    programação : ["linguagem","framework","mysql","react","nodejs"],
+    alimento : ["feijão","hamburguer","piza","batata","refrigerante"],
+}
