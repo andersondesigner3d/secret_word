b25463be740ebd623c03253ab8971d56199bda38
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2023-06-02 23:42:16 -0300

    sorteando palavra, categoria e quebrando a palavra em letras

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,56 @@ function App() {
   //não precisa do setWords pq ele não mudará
   const [words] = useState(wordsList);
 
-  console.log(words);
+  //variaveis que controlam palavras, categorias e letras
+  const [pickedWord,setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters,setLetters] = useState([]);
+
+  //função que sorteia palavra e categoria 
+  const pickWordAndCategory  = () =>{
+    //cria array com todas categorias usando as keys do objeto words
+    const categories = Object.keys(words);
+    //sorteia no array uma categoria
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    //sorteia palavra
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    //retorna em forma de objeto
+    return {word,category};
+  }
+
+  //starts secret word game
+  const startGame = () =>{
+    //pick word,category and letters
+    const {word,category} = pickWordAndCategory();
+    //quebra word em letras
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    console.log(category+" "+word+" "+wordLetters);
+
+    //seta as variaveis
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+    
+    //muda de pagina
+    setGameStage(stages[1].name);
+  }
+
+  //process the letter input
+  const verifyLetter = () =>{
+    setGameStage(stages[2].name);
+  }
+
+  //reinicia o jogo
+  const retry = () =>{
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className='App'>
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   )
 }
