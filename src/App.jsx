b16e01d9c34d2ff54d77314fdaf96f4c6c1a75bb
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2023-06-05 16:48:50 -0300

    adicionadas letras no array de letras corretas e de letras erradas

--- src/App.jsx
+++ src/App.jsx
@@ -70,13 +70,32 @@ function App() {
 
   //process the letter input
   const verifyLetter = (letter) => {
-    console.log(letter);
-  }
+    const normalizedLetter = letter.toLowerCase();
+    //check  if letter has already  been utilized
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)){
+      return;      
+    }
+    //push guessed letter or remove a guess
+    if(letters.includes(normalizedLetter)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+    }
+  };
+
+    console.log(guessedLetters);
+    console.log(wrongLetters);
 
   //reinicia o jogo
   const retry = () =>{
     setGameStage(stages[0].name);
-  }
+  };
 
   return (
     <div className='App'>
